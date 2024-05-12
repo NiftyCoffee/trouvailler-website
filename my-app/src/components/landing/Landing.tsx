890a3cf93156edca7cc9db13d8f01a8873038d44
NiftyCoffee <ztay0013@student.monash.edu>
2024-05-12 13:21:03 +1000

    Updated form link

--- my-app/src/components/landing/Landing.tsx
+++ my-app/src/components/landing/Landing.tsx
@@ -15,7 +15,7 @@ export default function Landing() {
                     <p className="p-landing">We want to build <span className="span-landing">bespoke, enriching</span> travel experiences for <span className="span-landing">everyone, everywhere.</span></p>
                     <div className="div-landing-btncontainer">
                         <button className="btn btn-landing-signup">
-                            <a href="https://ckzx98ogzda.typeform.com/to/AJqsbA3Z" target="_blank">Get Started</a>
+                            <a href="https://forms.gle/pviKSMMbSqa4uWGP6" target="_blank">Get Started</a>
                         </button>
                         <button className="btn btn-landing-findoutmore">
                             <a href="#section-contactus">Find Out More</a>
